951b5c2b8922d36e689b4613fd6ec1629918bbd2
Claudio Rojas Rodriguez <claudio@MacBook-Air-de-Claudio.local>
2017-07-25 01:46:25 -0400

    add new function and instrucctions

--- test/RUTfunctionsSpec.js
+++ test/RUTfunctionsSpec.js
@@ -1,70 +1,75 @@
 const assert = require('assert');
 const RUTfunctions = require('../src/index');
 
-describe('RUTfunctions Test', () => {
+describe('rutGetNumber Test', () => {
   describe('RUTnumber 1', () => {
     it("16.751.256-9 generate: 16751256'", () => {
-      assert.equal('16751256', RUTfunctions.RUTnumber('16.751.256-9'));
+      assert.equal('16751256', RUTfunctions.rutGetNumber('16.751.256-9'));
     });
   });
-  describe('RUTnumber 2', () => {
+  describe('rutGetNumber 2', () => {
     it("16751256-9 generate: 16751256'", () => {
-      assert.equal('16751256', RUTfunctions.RUTnumber('16751256-9'));
+      assert.equal('16751256', RUTfunctions.rutGetNumber('16751256-9'));
     });
   });
-  describe('RUTvalidate 1', () => {
+  describe('rutValidate 1', () => {
     it('16751256-9 generate: true', () => {
-      assert.equal(true, RUTfunctions.RUTvalidate('16.751.256-9'));
+      assert.equal(true, RUTfunctions.rutValidate('16.751.256-9'));
     });
   });
-  describe('RUTvalidate 2', () => {
+  describe('rutValidate 2', () => {
     it('16751256-9 generate: false', () => {
-      assert.equal(true, RUTfunctions.RUTvalidate('16751256-9'));
+      assert.equal(true, RUTfunctions.rutValidate('16751256-9'));
     });
   });
-  describe('RUTvalidate 3', () => {
+  describe('rutValidate 3', () => {
     it('1675125a6-9 generate: false', () => {
-      assert.equal(false, RUTfunctions.RUTvalidate('1675125a6-9'));
+      assert.equal(false, RUTfunctions.rutValidate('1675125a6-9'));
     });
   });
-  describe('RUTvalidate 4', () => {
+  describe('rutValidate 4', () => {
     it('17898425-k generate: true', () => {
-      assert.equal(true, RUTfunctions.RUTvalidate('17898425-k'));
+      assert.equal(true, RUTfunctions.rutValidate('17898425-k'));
     });
   });
-  describe('RUTvalidate 5', () => {
+  describe('rutValidate 5', () => {
     it('17898425-K generate: true', () => {
-      assert.equal(true, RUTfunctions.RUTvalidate('17898425-K'));
+      assert.equal(true, RUTfunctions.rutValidate('17898425-K'));
     });
   });
-  describe('RUTclean', () => {
+  describe('rutClean', () => {
     it('16.761.256-9 generate: 167512569', () => {
-      assert.equal('167512569', RUTfunctions.RUTclean('16.751.256-9'));
+      assert.equal('167512569', RUTfunctions.rutClean('16.751.256-9'));
     });
   });
-  describe('RUTformat', () => {
+  describe('rutFormat', () => {
     it('167512569 generate: 16.761.256-9', () => {
-      assert.equal('16.751.256-9', RUTfunctions.RUTformat('167512569'));
+      assert.equal('16.751.256-9', RUTfunctions.rutFormat('167512569'));
     });
   });
-  describe('RUTDv 1', () => {
+  describe('rutGetDv 1', () => {
     it('16.751.256-9 generate: 9', () => {
-      assert.equal('9', RUTfunctions.RUTDv('16.751.256-9'));
+      assert.equal('9', RUTfunctions.rutGetDv('16.751.256-9'));
     });
   });
-  describe('RUTDv 2', () => {
+  describe('rutGetDv 2', () => {
     it('16751256-9 generate: 9', () => {
-      assert.equal('9', RUTfunctions.RUTDv('16751256-9'));
+      assert.equal('9', RUTfunctions.rutGetDv('16751256-9'));
     });
   });
-  describe('RUTgetDv', () => {
+  describe('rutCalcDv', () => {
     it('16751256 generate: 9', () => {
-      assert.equal('9', RUTfunctions.RUTgetDv('16751256'));
+      assert.equal('9', RUTfunctions.rutCalcDv('16751256'));
     });
   });
-  describe('RUTNumAppendDv', () => {
+  describe('rutAddDv', () => {
     it('16751256 generate: 167512569', () => {
-      assert.equal('167512569', RUTfunctions.RUTNumAppendDv('16751256'));
+      assert.equal('167512569', RUTfunctions.rutAddDv('16751256'));
+    });
+  });
+  describe('Test deprecated', () => {
+    it('16.751.256-9 generate: 16751256', () => {
+      assert.equal('16.751.256-9', RUTfunctions.testDeprecated('16751256'));
     });
   });
 });
